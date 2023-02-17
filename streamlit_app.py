171d7f0751cdeeab7b2849a54ab1a8f511dc7ede
Medstaroc <111002461+Medstaroc@users.noreply.github.com>
2023-02-17 16:20:03 +0100

    added requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit 
 import pandas
+import requests
 
 
 streamlit.title("My Mom's New Healthy Dinner")
@@ -16,6 +17,8 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.header("Fruityvice Fruit Advice!")
+streamlit.text(fruityvice_response.json())
 
 

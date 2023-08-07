9ad4f81c10e7d17fed23cfa4f98bf36c8a3e46ba
Medstaroc <111002461+Medstaroc@users.noreply.github.com>
2023-08-07 16:04:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,14 @@
 import streamlit as st
 import pandas as pd
 
-# Simulation de données pour les groupes et l'historique des traitements
+# Simulation de données pour les groupes, jeux de données et l'historique des traitements
 groupes = ['Admin', 'Direction Déchets', 'Délibérations', 'Collectivités']
+jeux_de_donnees = {
+    'Admin': ['Données A', 'Données B', 'Données C'],
+    'Direction Déchets': ['Déchets A', 'Déchets B', 'Déchets C'],
+    'Délibérations': ['Décision 1', 'Décision 2', 'Décision 3'],
+    'Collectivités': ['Ville A', 'Ville B', 'Ville C']
+}
 historique = pd.DataFrame({
     'Date': ['2023-08-01 10:00', '2023-08-02 15:30'],
     'Utilisateur': ['admin_user', 'dechets_user'],
@@ -29,8 +35,16 @@ def afficher_tableau_de_bord_admin():
     st.title('Tableau de Bord - Admin')
     st.sidebar.title('Options')
     
-    # Section de gestion des mises à jour (à compléter)
+    # Section de gestion des mises à jour
+    st.header('Gestion des Mises à Jour')
+    groupe = 'Admin'
     
+    if groupe in jeux_de_donnees:
+        jeu_de_donnees_selectionne = st.selectbox('Sélectionner un jeu de données', jeux_de_donnees[groupe])
+        if st.button('Mettre à jour'):
+            # Appeler l'API opendatasoft explore pour mettre à jour le jeu de données sélectionné
+            # Ajouter des fonctionnalités supplémentaires ici
+
     # Section d'historique
     st.sidebar.button('Historique', afficher_historique)
 
@@ -39,8 +53,16 @@ def afficher_tableau_de_bord_dechets():
     st.title('Tableau de Bord - Direction Déchets')
     st.sidebar.title('Options')
     
-    # Section de gestion des mises à jour (à compléter)
+    # Section de gestion des mises à jour
+    st.header('Gestion des Mises à Jour')
+    groupe = 'Direction Déchets'
     
+    if groupe in jeux_de_donnees:
+        jeu_de_donnees_selectionne = st.selectbox('Sélectionner un jeu de données', jeux_de_donnees[groupe])
+        if st.button('Mettre à jour'):
+            # Appeler l'API opendatasoft explore pour mettre à jour le jeu de données sélectionné
+            # Ajouter des fonctionnalités supplémentaires ici
+
     # Section d'historique
     st.sidebar.button('Historique', afficher_historique)
 
@@ -48,20 +70,10 @@ def afficher_tableau_de_bord_dechets():
 def afficher_historique():
     st.title('Historique des Traitements')
     st.dataframe(historique)
-# Section de gestion des mises à jour
-st.header('Gestion des Mises à Jour')
-groupe = st.session_state.groupe  # Récupérer le groupe de l'utilisateur connecté
 
-if groupe:
-    jeu_de_donnees_selectionne = st.selectbox('Sélectionner un jeu de données', jeux_de_donnees[groupe])
-    if st.button('Mettre à jour'):
-        # Appeler l'API opendatasoft explore pour mettre à jour le jeu de données sélectionné
-        # Ajouter des fonctionnalités supplémentaires ici
 # Page principale
 def main():
     authentification()
-
-
 
 if __name__ == '__main__':
     main()

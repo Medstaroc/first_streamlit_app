90da3c442055da2c66b1859b1a437f01ec51c536
Medstaroc <111002461+Medstaroc@users.noreply.github.com>
2023-08-08 18:11:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,35 +5,35 @@ import streamlit as st
 # Page d'authentification
 def login():
     st.title("Authentification")
-    username = st.text_input("Nom d'utilisateur")
-    password = st.text_input("Mot de passe", type="password")
+    utilisateur = st.text_input("Nom d'utilisateur")
+    mot_de_passe = st.text_input("Mot de passe", type="password")
     submit_button = st.button("Se connecter")
     
     if submit_button:
         # Vérifier les informations d'identification
-        if username == "mohamed.darari@nantesmetropole.fr" and password == "b8RmmgY5":
+        if utilisateur == "mohamed.darari@nantesmetropole.fr" and mot_de_passe == "b8RmmgY5":
             st.session_state.authenticated = True
             st.success("Authentification réussie. Vous pouvez maintenant mettre à jour les jeux de données.")
         else:
             st.error("Nom d'utilisateur ou mot de passe incorrect.")
 
 # Fonction pour mettre à jour les jeux de données
-def update_dataset(username, password, selected_dataset, uploaded_file):
+def update_dataset(utilisateur, mot_de_passe, selected_dataset, uploaded_file):
     # Code pour mettre à jour les jeux de données sur le portail de Nantes Métropole
     dataset_id = selected_dataset
-    dataset_uid = get_dataset_uid(dataset_id, username, password)
+    dataset_uid = get_dataset_uid(dataset_id, utilisateur, mot_de_passe)
     
     upload_url = "https://data.nantesmetropole.fr/api/management/v2/files"
     uploaded_file_name = uploaded_file.name
     
     with open(uploaded_file_name, 'rb') as file:
         files = {'file': (uploaded_file_name, file)}
-        response = requests.post(upload_url, files=files, auth=(username, password))
+        response = requests.post(upload_url, files=files, auth=(utilisateur, mot_de_passe))
     data = response.json()
     url_res = data['url']
     
     url = f'https://data.nantesmetropole.fr/api/management/v2/datasets/{dataset_uid}/resources/'
-    response = requests.get(url, auth=(username, password))
+    response = requests.get(url, auth=(utilisateur, mot_de_passe))
     data = response.json()
     resource_uid = data[0]["resource_uid"]
     
@@ -52,10 +52,10 @@ def update_dataset(username, password, selected_dataset, uploaded_file):
             "doublequote": True
         }
     }
-    response = requests.put(url, json=payload, auth=(username, password))
+    response = requests.put(url, json=payload, auth=(utilisateur, mot_de_passe))
     
     url = f'https://data.nantesmetropole.fr/api/management/v2/datasets/{dataset_uid}/publish/'
-    response = requests.put(url, auth=(username, password))
+    response = requests.put(url, auth=(utilisateur, mot_de_passe))
     
     return response
 
@@ -85,7 +85,7 @@ def main():
             uploaded_file = st.file_uploader("Charger le fichier à jour", type=["csv"])
 
             if uploaded_file:
-                update_response = update_dataset(username, password, selected_dataset, uploaded_file)
+                update_response = update_dataset(utilisateur, mot_de_passe, selected_dataset, uploaded_file)
                 
                 if update_response.status_code == 200:
                     st.success("Mise à jour réussie !")
